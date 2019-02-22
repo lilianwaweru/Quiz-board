93380da0e9d43ec08c65ae923cc4b8787ac0436f
lilian waweru <lilowesh.lw@gmail.com>
2019-02-22 17:32:35 +0300

    function in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
-//business logic
-
-var question1 = $("input:radio[name=que1]:checked").val();
-var question2 = &("input:radio[name=que2]:checked").val();
-var question3 = &("input:radio[name=que3]:checked").val();
-var question4 = &("input:radio[name=que4]:checked").val();
-var sum=function(question1, question2, question3, question4){
+//Business logic
+function sum (question1, question2, question3, question4){
  return (question1+question2+question3+question4);
-alert( sum);
 }
+
+//User logic
+$(document).ready(function(){
+  $("#question").submit(function(event) {
+    event.preventDefault();
+var question1 =parseInt($("input:radio[name=que1]:checked").val());
+var question2 =parseInt($("input:radio[name=que2]:checked").val());
+var question3 = parseInt($("input:radio[name=que3]:checked").val());
+var question4 =parseInt($("input:radio[name=que4]:checked").val());
+var total = sum(question1, question2, question3, question4);
+
+// var percent = (total/40)*100;
+//alert(percent + "%");
+// if (percent>80 && percent <=100) {
+  // alert("Excellent")
+// } else if(percent<80 && percent>=50){
+  // alert("fairly passed");
+// } else {
+  // alert("failed!Retake");
+// }
+});
+});
