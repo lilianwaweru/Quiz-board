9a28f21f7c79f4d00d22c45d7f854b7bd4cd89a1
lilian waweru <lilowesh.lw@gmail.com>
2019-02-25 07:36:28 +0300

    .hide and .show

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,35 @@
 //Business logic
 function sum (question1, question2, question3, question4){
- return (question1+question2+question3+question4);
+  return (question1+question2+question3+question4);
 }
 
 //User logic
+
+
 $(document).ready(function(){
+  $('#details').submit(function (event) {
+    event.preventDefault();
+    $("#details").hide();
+    $("#question").show();
+  })
   $("#question").submit(function(event) {
     event.preventDefault();
-var question1 =parseInt($("input:radio[name=que1]:checked").val());
-var question2 =parseInt($("input:radio[name=que2]:checked").val());
-var question3 = parseInt($("input:radio[name=que3]:checked").val());
-var question4 =parseInt($("input:radio[name=que4]:checked").val());
-var total = sum(question1, question2, question3, question4);
+    $("#question").hide();
 
-var percent = (total/40)*100;
-$("#score").text(percent);
-if (percent>80 && percent <=100) {
-  $("#score").text("excelent");
-} else if(percent<80 && percent>=50){
-  $("#score").text("fairly passed");
-} else {
-  $("#score").text("failed!retake the test");
-}
-});
+    var question1 =parseInt($("input:radio[name=que1]:checked").val());
+    var question2 =parseInt($("input:radio[name=que2]:checked").val());
+    var question3 = parseInt($("input:radio[name=que3]:checked").val());
+    var question4 =parseInt($("input:radio[name=que4]:checked").val());
+    var total = sum(question1, question2, question3, question4);
+
+    var percent = (total/40)*100;
+    $("#score").text(percent);
+    if (percent>80 && percent <=100) {
+      $("#score").text("excelent");
+    } else if(percent<80 && percent>=50){
+      $("#score").text("fairly passed");
+    } else {
+      $("#score").text("failed!retake the test");
+    }
+  });
 });
